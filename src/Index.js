9e43786b94d8297161c05c2820b395437961057d
Ettikus <maraisettienne@gmail.com>
2023-11-07 22:55:45 +0200

    chnages

--- src/Index.js
+++ src/Index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css'; 
-import App from './Components/BudgetApp'; 
+import './Index.css'; 
+import App from './Components/budgetapp'; 
 
 ReactDOM.render(
   <React.StrictMode>
