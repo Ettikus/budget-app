f5b5f4f220dc3c51e42dc87bad09d81fc038afc4
Ettikus <maraisettienne@gmail.com>
2023-11-07 22:31:24 +0200

    changes

---
+++ src/Index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'; // your global styles
+
+import App from './Components/budgetapp'; 
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
